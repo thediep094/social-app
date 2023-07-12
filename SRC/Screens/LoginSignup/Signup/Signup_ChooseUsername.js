2939c0857aefaa469f389844ccaf93c63c000e09
huybq_107 <64139999+huybq100701@users.noreply.github.com>
2023-07-12 21:00:22 +0700

    frontend

--- SRC/Screens/LoginSignup/Signup/Signup_ChooseUsername.js
+++ SRC/Screens/LoginSignup/Signup/Signup_ChooseUsername.js
@@ -1,41 +1,87 @@
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { containerFull, goback, logo1 } from '../../../CommonCss/pagecss'
-import {MaterialIcons} from '@expo/vector-icons'
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
+import { containerFull, goback, hr80, logo1 } from '../../../CommonCss/pagecss'
 import logo from '../../../../assets/logo.png'
-import { formHead2, formInput, formbtn } from '../../../CommonCss/formcss'
+import { formbtn, formHead, formHead2, formHead3, formInput, formTextLinkCenter, formTextLinkRight } from '../../../CommonCss/formcss'
+import { MaterialIcons } from '@expo/vector-icons';
+const Signup_ChooseUsername = ({ navigation, route }) => {
+    const { email } = route.params
+    const [username, setusername] = useState('')
+
+    const [loading, setLoading] = useState(false)
 
 
-const Signup_ChooseUsername = ({navigation}) => {
-  return (
-    <View style={containerFull}>
-    <TouchableOpacity onPress={() => navigation.navigate('Login')} style={goback}>
+    const handleUsername = () => {
+        if (username == '') {
+            alert('Please enter username')
+        }
+        else {
+            setLoading(true)
+            fetch('http://10.0.2.2:3000/changeusername', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    username: username
+                })
+            })
+                .then(res => res.json()).then(
+                    data => {
+                        if (data.message === "Username Available") {
+                            setLoading(false)
+                            alert('Username has been set successfully')
+                            navigation.navigate('Signup_ChoosePassword', { email: email, username: username })
+                        }
+                        else {
+                            setLoading(false)
+                            alert("Username not available");
+                        }
+                    }
+                ).catch(err => {
+                    console.log(err)
+                })
 
-        <MaterialIcons name="arrow-back-ios" size={24} color="gray" />
-        <Text style={{
-            color: 'gray',
-            fontSize: 16,
-        }}
+        }
 
-        >Go Back</Text>
+        // navigation.navigate('Signup_ChoosePassword')
+    }
 
-    </TouchableOpacity>
+    return (
+        <View style={containerFull}>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')} style={goback}>
 
-    <Image source={logo} style={logo1} />
-    <Text style={formHead2}>Choose a Username</Text>
-    <TextInput placeholder="Enter Your Username" style={formInput}
+                <MaterialIcons name="arrow-back-ios" size={24} color="gray" />
+                <Text style={{
+                    color: 'gray',
+                    fontSize: 16,
+                }}
 
-    onChangeText={(text) => {
-    setEmail(text)
-}}
-/>
-    <Text style={formbtn}
-    onPress={() => navigation.navigate
-    ('Signup_ChoosePassword')}
-    > Next</Text>
-    </View>
-  )
+                >Go Back</Text>
+
+            </TouchableOpacity>
+
+            <Image source={logo} style={logo1} />
+            <Text style={formHead2}>Choose a Username</Text>
+            <TextInput placeholder="Enter a username" style={formInput}
+                onChangeText={(text) => setusername(text)}
+            />
+
+            {
+                loading ? <ActivityIndicator /> :
+                    <Text style={formbtn}
+                        onPress={() => handleUsername()}
+                    >
+                        Next
+                    </Text>
+            }
+        </View>
+    )
 }
+
+
+
 
 export default Signup_ChooseUsername
 
--- SRC/Screens/LoginSignup/Signup/Signup_EnterVerificationCode.js
+++ SRC/Screens/LoginSignup/Signup/Signup_EnterVerificationCode.js
@@ -1,39 +1,64 @@
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { containerFull, goback, logo1 } from '../../../CommonCss/pagecss'
-import {MaterialIcons} from '@expo/vector-icons'
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { containerFull, goback, hr80, logo1 } from '../../../CommonCss/pagecss'
 import logo from '../../../../assets/logo.png'
-import { formHead2, formHead3, formInput, formbtn } from '../../../CommonCss/formcss'
+import { formbtn, formHead, formHead2, formHead3, formInput, formTextLinkCenter, formTextLinkRight } from '../../../CommonCss/formcss'
+import { MaterialIcons } from '@expo/vector-icons';
+const Signup_EnterVerificationCode = ({ navigation, route }) => {
+    const { useremail, userVerificationCode } = route.params
+    console.log(useremail, userVerificationCode)
 
 
-const Signup_EnterVerificationCode = ({navigation}) => {
-  return (
-    <View style={containerFull}>
-    <TouchableOpacity onPress={() => navigation.navigate('Login')} style={goback}>
+    const [verificationCode, setVerificationCode] = useState('')
 
-        <MaterialIcons name="arrow-back-ios" size={24} color="gray" />
-        <Text style={{
-            color: 'gray',
-            fontSize: 16,
-        }}
+    const handleVerificationCode = () => {
+        if (verificationCode != userVerificationCode) {
+            alert('Invalid Verification Code')
+        }
+        else if (verificationCode == userVerificationCode) {
+            alert('Verification Code Matched')
+            navigation.navigate('Signup_ChooseUsername', { email: useremail })
+        }
+        else {
+            alert('Please Try Again')
+        }
 
-        >Go Back</Text>
+        // navigation.navigate('Signup_ChooseUsername')
+    }
 
-    </TouchableOpacity>
+    return (
+        <View style={containerFull}>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')} style={goback}>
 
-    <Image source={logo} style={logo1} />
-    <Text style={formHead3}>
-      A verification code has been sent to your email
-    </Text>
-    <TextInput placeholder="Enter 6-digit code here" style={formInput}
-/>
-    <Text style={formbtn}
-    onPress={() => navigation.navigate
-    ('Signup_ChooseUsername')}
-    > Next</Text>
-    </View>
-  )
+                <MaterialIcons name="arrow-back-ios" size={24} color="gray" />
+                <Text style={{
+                    color: 'gray',
+                    fontSize: 16,
+                }}
+
+                >Go Back</Text>
+
+            </TouchableOpacity>
+
+            <Image source={logo} style={logo1} />
+            <Text style={formHead3}>
+                A verification code has been sent to your email
+            </Text>
+            <TextInput placeholder="Enter 6-Digit Code here" style={formInput}
+
+                onChangeText={(text) => setVerificationCode(text)}
+            />
+
+            <Text style={formbtn}
+                onPress={() => handleVerificationCode()}
+            >
+                Next
+            </Text>
+        </View>
+    )
 }
+
+
 
 export default Signup_EnterVerificationCode
 
